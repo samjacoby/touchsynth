917d1af7ac641bb290941b89de69b2e16462c8db
Sam Jacoby <sjacoby@media.mit.edu>
2013-01-21 19:18:46 -0500

    Adding after name change, along with new header file.

---
+++ touch_sound.h
@@ -0,0 +1,8 @@
+#ifndef __touch_sound_h_
+#define __touch_sound_h_
+
+#define LED1 PINF0 
+#define LED2 PINF1 
+
+#endif // __touch_sound_h 
+
---
+++ touch_sound.ino
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <avr/io.h>
+#include <avr/interript.h>
+#include <CapSense.h>
+
+#include "touch_sound.h"
+#include "audio.h"
+#include "sinetable.h"
+#include "synth.h"
+
+CapSense clip_one = CapSense(9, 10);
+CapSense clip_two = CapSense(4, 12);
+CapSense clip_three = CapSense(13, 5);
+
+uint8_t next_sample = 0;
+
+void setup() 
+{
+
+   DDRF |= (1 << LED1) | (1 << LED2);
+   cli();
+   audio_init();
+   //synth_init();
+   sei();
+
+   Serial.begin(115200);
+
+}
+
+long total1, total2, total3;
+char * data_str = (char*) malloc(18 * sizeof(char));
+
+void loop() {
+/*
+    total1 =  clip_one.capSense(5);
+    total2 =  clip_two.capSense(5);
+    total3 =  clip_three.capSense(5);
+
+    sprintf(data_str, "%lu, %lu, %lu", total1, total2, total3);
+    
+    delay(10);
+    uint16_t note = 127;
+    */
+
+    synth_generate(1000); 
+
+}
