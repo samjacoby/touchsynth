c4db900ce44229287a8b3a0647efa4ee668a94d8
Sam Jacoby <sjacoby@media.mit.edu>
2013-04-11 15:07:03 -0400

    Fixing LEDs.

--- led.cpp
+++ led.cpp
@@ -29,19 +29,23 @@ void setLED(uint8_t nLed, uint8_t val) {
                 PORTD |= (1 << C); 
                 break;
             case 3: // LED0
-                DDRD |= (1 << A) | (1 << B); 
+                    DDRD |= (1 << A) | (1 << B);
                 PORTD |= (1 << A); 
+                //  DDRD |= (1 << A) | (1 << B); 
+                //PORTD |= (1 << B); 
                 break;
             case 4: // LED1
-                DDRD |= (1 << B) | (1 << C);
+                            DDRD |= (1 << B) | (1 << C); 
                 PORTD |= (1 << B); 
                 break;
             case 5: // LED2
-                DDRD |= (1 << A) | (1 << B); 
+
+                    DDRD |= (1 << B) | (1 << A);
                 PORTD |= (1 << B); 
                 break;
             case 6: // LED3
-                DDRD |= (1 << B) | (1 << C);
+          
+                  DDRD |= (1 << B) | (1 << C); 
                 PORTD |= (1 << C); 
                 break;
 
